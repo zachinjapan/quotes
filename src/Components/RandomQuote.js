b691935d466457f2b508c135ac96bd7cade6009b
Zach Stone <zachinjapan888@gmail.com>
2021-11-30 01:07:39 -0500

    twitter share

--- src/Components/RandomQuote.js
+++ src/Components/RandomQuote.js
@@ -1,11 +1,20 @@
 import "./RandomQuote.css";
 
 const RandomQuote = (props) => {
+  let twitterUrl =
+    "https://twitter.com/intent/tweet?text=" +
+    props.quote +
+    " - " +
+    props.author;
+
   return (
     <div className="random-quote">
       <p className="random-quote-text">"{props.quote}"</p>
       {/* just for testing */}
       <h1> {props.author}</h1>
+      <a class="twitter-share-button" href={twitterUrl}>
+        Tweet
+      </a>
     </div>
   );
 };
