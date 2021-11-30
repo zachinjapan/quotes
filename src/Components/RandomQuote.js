df19d0ba8a8524bde542d8779e4366411ae4a4ef
Zach Stone <zachinjapan888@gmail.com>
2021-11-30 09:58:22 -0500

    add url to twiter post sharing feat

--- src/Components/RandomQuote.js
+++ src/Components/RandomQuote.js
@@ -1,20 +1,25 @@
-import './RandomQuote.css';
-import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./RandomQuote.css";
+import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const RandomQuote = (props) => {
-	let twitterUrl = 'https://twitter.com/intent/tweet?text=' + props.quote + ' - ' + props.author;
+  let twitterUrl =
+    "https://twitter.com/intent/tweet?text=" +
+    props.quote +
+    " - " +
+    props.author +
+    "&url=https://quotequiz.netlify.app";
 
-	return (
-		<div className="random-quote">
-			<p className="random-quote-text">"{props.quote}"</p>
-			{/* just for testing */}
-			<h1> {props.author}</h1>
-			<a class="twitter-share-button" href={twitterUrl}>
-				<FontAwesomeIcon icon={faTwitterSquare} size="3x" spin />
-			</a>
-		</div>
-	);
+  return (
+    <div className="random-quote">
+      <p className="random-quote-text">"{props.quote}"</p>
+      {/* just for testing */}
+      <h1> {props.author}</h1>
+      <a class="twitter-share-button" href={twitterUrl}>
+        <FontAwesomeIcon icon={faTwitterSquare} size="3x" spin />
+      </a>
+    </div>
+  );
 };
 
 export default RandomQuote;
