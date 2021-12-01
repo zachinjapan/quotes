3aaa584e08e845b1494359e0970d0bc6e3a09216
Clifford Morin <cemorin21@gmail.com>
2021-12-01 02:46:10 -0500

    added new fonts and other fancy stuff

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,7 +1,17 @@
-import "./Title.css";
+import './Title.css';
 
 const Title = () => {
-  return <h1 className="title">Guess Who said this famous quote</h1>;
+	return (
+		<div>
+			<h1 className="title">
+				<span />
+				<span />
+				<span />
+				<span />
+				Guess Who said this Famous Quote
+			</h1>
+		</div>
+	);
 };
 
 export default Title;
